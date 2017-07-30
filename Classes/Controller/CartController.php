ef0801181b4d284d183aebde64812075a3a5dbef
Patric Eckhart <mail@patriceckhart.com>
2017-07-30 18:44:19 +0200

    no message

---
+++ Classes/Controller/CartController.php
@@ -0,0 +1,24 @@
+<?php
+namespace NeosCommerce\Cart\Controller;
+
+/*
+ * This file is part of the NeosCommerce.Cart package.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Controller\ActionController;
+
+class CartController extends ActionController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $this->view->assign('foos', array(
+            'bar', 'baz'
+        ));
+    }
+
+}
