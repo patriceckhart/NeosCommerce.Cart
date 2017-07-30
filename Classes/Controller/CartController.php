890b20ac487058d04bd45c5308bbfa915dfdc31b
Patric Eckhart <mail@patriceckhart.com>
2017-07-30 19:11:18 +0200

    no message

--- Classes/Controller/CartController.php
+++ Classes/Controller/CartController.php
@@ -8,17 +8,44 @@ namespace NeosCommerce\Cart\Controller;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\Controller\ActionController;
 
+/**
+ * @Flow\Scope("session")
+ */
 class CartController extends ActionController
 {
 
     /**
+     * @var array
+     */
+    protected $items = array();
+
+    /**
+     * @param array $item
+     * @return void
+     * @Flow\Session(autoStart = TRUE)
+     */
+    public function addItemAction($item) {
+        $this->items[] = $item;
+        $this->redirectToUri('/');
+    }
+
+    /**
      * @return void
      */
-    public function indexAction()
-    {
-        $this->view->assign('foos', array(
-            'bar', 'baz'
-        ));
+    public function miniCartAction() {
+        $cart = $this->items;
+        $cartcount = count($cart);
+        if ($cartcount>0) {
+            $sum = FALSE;
+            $sum = intval($sum);
+            foreach ($cart as $dat) {
+                $quantity = intval($dat["quantity"]);
+                $sum += $quantity;
+            }
+            $this->view->assign('itemscount', $sum);
+        } else {
+            $this->view->assign('itemscount', '0');
+        }
     }
 
 }
